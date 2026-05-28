a95b439bc05a1b52c7f915d11dc1064089218232
Naveenkumar-76 <nnaveenkumar7671@gmail.com>
2026-05-28 17:47:09 +0530

    Added solved prime check problems

---
+++ Java_Progress/primecheck/Primecheck_1.java
@@ -0,0 +1,41 @@
+package primecheck;
+import java.util.Scanner;
+
+public class Primecheck_1 {
+	
+	static String primecheck(int n) {
+		
+		if(n <= 0) {
+			return "Invalid";
+		}
+		
+		int count = 0;
+		
+//		Logic for prime number check
+		for(int i = 1; i <= n; i++) {
+			if(n%i == 0) {
+				count++;
+			}
+		}
+		if(count == 2) {
+			return "Prime number";
+		} else {
+			return "Not a prime number";
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+//		User input
+		System.out.print("Enter a number: ");
+		int n = sc.nextInt();
+		
+//		output
+		String res = primecheck(n);
+		System.out.println(res);
+		
+		sc.close();
+	}
+	
+}
---
+++ Java_Progress/primecheck/Primecheck_3.java
@@ -0,0 +1,36 @@
+package primecheck;
+import java.util.Scanner;
+
+public class Primecheck_3 {
+	
+	static String primecheck(int n) {
+		
+		if(n <= 0) {
+			return "Invalid";
+		}
+		
+//		Logic for prime number check
+		for(int i = 2; i*i <= n; i++) {
+			if(n%i == 0) {
+				return "Not a prime number";
+			}
+		}
+		return "Prime number";
+
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+//		User input
+		System.out.print("Enter a number: ");
+		int n = sc.nextInt();
+		
+//		output
+		String res = primecheck(n);
+		System.out.println(res);
+		
+		sc.close();
+	}
+	
+}
